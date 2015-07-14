0a947219b8e1c7a1c886e28f8455a2eb5ae76980
nathaned <contactnathand@gmail.com>
2015-07-14 13:13:04 -0400

    calculateHandlen
    implemented calculatedHandlen

--- Word Game/ps4a.py
+++ Word Game/ps4a.py
@@ -195,7 +195,10 @@ def calculateHandlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
+    total = 0
+    for val in hand.values():
+        total += val
+    return total
 
 
 
